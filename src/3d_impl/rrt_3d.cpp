d686370d07b927059a9091bd7dcbe6d1dd6832ab
Naomi Gonzalez <ngonzalz@bu.edu>
2025-04-26 13:28:44 -0400

    steer func impl

--- src/3d_impl/rrt_3d.cpp
+++ src/3d_impl/rrt_3d.cpp
@@ -6,6 +6,10 @@
 #include <limits>    
 #include <iostream>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 RRTStar3D::RRTStar3D(int seed, const std::vector<SphereObstacle>& _obstacles, const std::vector<std::pair<double, double>> _rad_limits)
     : GoalBiasedGreedySteerKNeighborhoodRRTStarBase(seed), 
       obstacles(_obstacles),          
@@ -63,8 +67,63 @@ double RRTStar3D::distance(const Configuration& c1, const Configuration& c2){
 }
 
 Configuration RRTStar3D::steer(const Configuration& c0, const Configuration& c, double step_size){
-	return {};
+	// If points are the same or very close just return orginal joint point
+    if (allclose(c0, c)) return c0;
+
+    // Find distance and normalize each step to find step size 
+    double total_dist = distance(c0, c);
+    Configuration step_vec(dof);
+    for (int i = 0; i < dof; ++i) {
+        double diff = c[i] - c0[i];
+
+        // In case angle wraps around take the shorter one
+        double normalized_diff = std::remainder(diff, 2.0 * M_PI);
+        step_vec[i] = normalized_diff * (step_size / total_dist);
+    }
+
+    Configuration curr_point = c0;
+    bool made_step = false;
+
+    // Slowly step towards the target c
+    int max_iterations = static_cast<int>(total_dist / step_size) + 2;
+    for (int i = 0; i < max_iterations; ++i){
+        double dist_left = distance(curr_point, c);
+        if (dist_left < 0) break;
+
+        // Check if we can go to c if distance left is small
+        if (dist_left <= step_size){
+            // If target c is not a vaild point break loop 
+            if (!valid(c)) break;
+
+            // Make final step if collision free 
+            if (collision_free(curr_point, c, step_size)) {
+                curr_point = c;
+                made_step = true;
+            }
+            break;
+        }
+
+        // If not find next potential point and try going to that point
+        else{
+            Configuration next_point(dof);
+            for (int j = 0; j < dof; ++j) {
+                next_point[j] = curr_point[j] + step_vec[j];
+            }
+
+            if (!collision_free(curr_point, next_point, step_size)) break;
+            else {
+                curr_point = next_point;
+                made_step = true;
+            }
+
+        }
+    }
+
+    // Return closet point to target c or nothing if not able to make any steps
+    if (made_step) return curr_point;
+    else return Configuration();
 }
+
 
 bool RRTStar3D::allclose(const Configuration& c1, const Configuration& c2){
 	if((c1.size() != dof) || (c2.size() != dof)){
