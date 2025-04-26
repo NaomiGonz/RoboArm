52b6b45ffea58faa9b17b1d5eab0ec78dfb5a053
GidonG <gautelg@bu.edu>
2025-04-25 23:36:07 -0400

    added distance & allclose

--- src/3d_impl/rrt_3d.cpp
+++ src/3d_impl/rrt_3d.cpp
@@ -4,6 +4,7 @@
 #include <random>   
 #include <stdexcept> 
 #include <limits>    
+#include <iostream>
 
 RRTStar3D::RRTStar3D(int seed, const std::vector<SphereObstacle>& _obstacles, const std::vector<std::pair<double, double>> _rad_limits)
     : GoalBiasedGreedySteerKNeighborhoodRRTStarBase(seed), 
@@ -20,11 +21,24 @@ RRTStar3D::RRTStar3D(int seed, const std::vector<SphereObstacle>& _obstacles, co
 }
 
 Configuration RRTStar3D::sample(double p /* p = goal bias probability */){
-	return {};
+	
 }
 
+// returns negative if error
 double RRTStar3D::distance(const Configuration& c1, const Configuration& c2){
-	return 0;
+    if((c1.size() != dof) || (c2.size() != dof)){
+        std::cerr << "Configurations must equal degrees of freedom";
+        return -1;
+    }
+    
+    double sum_sq = 0.0;
+
+    for(int ii = 0; ii < dof; ii++){
+        double diff = c1[ii] - c2[ii];
+        sum_sq += diff*diff;
+    }
+
+    return std::sqrt(sum_sq);
 }
 
 Configuration RRTStar3D::steer(const Configuration& c0, const Configuration& c, double step_size){
@@ -32,7 +46,21 @@ Configuration RRTStar3D::steer(const Configuration& c0, const Configuration& c, 
 }
 
 bool RRTStar3D::allclose(const Configuration& c1, const Configuration& c2){
-	return 0;
+	if((c1.size() != dof) || (c2.size() != dof)){
+        std::cerr << "Configurations must equal degrees of freedom";
+        return false;
+    }
+    
+    // two tolerances are more adaptable when working in 6dof
+    const double atol = 1e-5; // absolute tolerance
+    const double rtol = 1e-3; // relative tolerance
+
+    for(int ii = 0; ii < dof; ii++){
+        if(std::fabs(c1[ii] - c2[ii]) > atol + rtol * std::fabs(c2[ii])){
+            return false;
+        }
+    }
+    return true;
 }
 
 bool RRTStar3D::valid(const Configuration& c){
