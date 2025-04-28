7a2085b1cbc4ed75b60fb135dc814ac22df699d9
GidonG <gautelg@bu.edu>
2025-04-28 11:06:24 -0400

    added obstacles

--- src/3d_impl/rrt_3d.cpp
+++ src/3d_impl/rrt_3d.cpp
@@ -490,7 +490,14 @@ int main() {
     //const double XYZ_MAX = 2.0;
 
     std::vector<SphereObstacle> obstacles;
-    obstacles.emplace_back(SphereObstacle{0.073214 , 0.0906615, 0.2217195, 0.05038023});
+    obstacles.emplace_back(SphereObstacle{0.14 , 0.05, 0.4, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.24 , 0.05, 0.4, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.14 , 0.0025, 0.4, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.14 , 0.0025, 0.3, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.0 , 0.05, 0.3, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.0 , 0.05, 0.2, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.1 , 0.04, 0.3, 0.05});
+    obstacles.emplace_back(SphereObstacle{0.1 , 0.04, 0.2, 0.05});
 
     // Joint angle restrictions in radians
     std::vector<std::pair<double, double>> joint_limits = { {-2.8973, 2.8973}, {-1.7628, 1.7628}, {-2.8973, 2.8973}, {-3.0718, -0.0698}, {-2.8973, 2.8973} };
